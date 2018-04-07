205fb8e64ad57441b6ea9900184d61d39a692c22
Eivind Samuelsen <eivind.samuelsen@gmail.com>
2018-04-07 16:13:25 +0200

    More hello world stuff

--- src/draftex.cpp
+++ src/draftex.cpp
@@ -1,11 +1,27 @@
+
+
 #include <oui_window.h>
+#include <oui_text.h>
 
-oui::window::Description oui::window::initialize()
+#pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
+
+
+int main()
 {
-	return { "draftex", 1280, 720 };
-}
+	oui::Window window{ { "draftex", 1280, 720 } };
 
-void oui::window::update(oui::Rectangle area, oui::Input& input)
-{
+	oui::Font font("Segoe UI", int(24*window.dpiFactor()));
 
+	while (window.update())
+	{
+		window.clear(oui::colors::white);
+		auto area = window.area();
+		
+		font.drawText(area, "hello", oui::colors::black);
+
+		oui::fill(area.popBottom(40), oui::colors::blue);
+	}
+
+
+	return 0;
 }
